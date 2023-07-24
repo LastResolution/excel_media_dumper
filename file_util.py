e5ffefe71f3e607b262a5ab1d0449bef67ecdbb0
LifeViolation <128378329+LifeViolation@users.noreply.github.com>
2023-07-24 22:59:01 +0900

    add: file dump function

--- file_util.py
+++ file_util.py
@@ -1,5 +1,7 @@
+import io
 import os
 import shutil
+import zipfile
 
 
 def initialize() -> None:
@@ -31,10 +33,14 @@ def is_xlsx(path: str) -> bool:
 def get_media(path: str) -> bool:
     file_name: str = __get_file_name(path)
     __copy_file(path, file_name)
+    __copy_media(path, file_name)
 
 
-def __get_file_name(path: str) -> str:
-    return os.path.splitext(os.path.basename(path))[0]
+def __get_file_name(path: str, with_extension: bool = False) -> str:
+    if with_extension:
+        return os.path.basename(path)
+    else:
+        return os.path.splitext(os.path.basename(path))[0]
 
 
 def __copy_file(path: str, file_name: str) -> None:
@@ -43,3 +49,44 @@ def __copy_file(path: str, file_name: str) -> None:
     )
 
     shutil.copy(path, copy_name)
+
+
+def __copy_media(path: str, file_name: str) -> None:
+    print(f"Start dump > {path}")
+    if not os.path.exists(f"resources{os.sep}output{os.sep}{file_name}"):
+        os.makedirs(f"resources{os.sep}output{os.sep}{file_name}")
+
+    try:
+        zip: zipfile.ZipFile = zipfile.ZipFile(
+            f"resources{os.sep}input{os.sep}{file_name}.zip", "r"
+        )
+    except Exception as e:
+        print("Failed dump.")
+        print(e)
+        print("Please make sure you have the correct .xlsx file.")
+        print("\n")
+        return
+
+    file_list: list[zipfile.ZipInfo] = zip.infolist()
+
+    media_count: int = 0
+    for file in file_list:
+        if file.filename.startswith("xl/media/"):
+            save_name: str = __get_file_name(file.filename, with_extension=True)
+            with zip.open(file.filename) as image:
+                img_bin = io.BytesIO(image.read())
+                with open(
+                    f"resources{os.sep}output{os.sep}{file_name}{os.sep}{save_name}",
+                    "wb",
+                ) as save_file:
+                    try:
+                        save_file.write(img_bin.getbuffer())
+                    except Exception as e:
+                        print("File save failed.")
+                        print("\n")
+
+                    media_count += 1
+                    print(f"Saving... {media_count}files.")
+    print(f"Save {media_count} files.")
+    print(f"Output > resources{os.sep}output{os.sep}{file_name}{os.sep}{save_name}")
+    print("\n")
