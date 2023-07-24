548e959ae5dc525d4bbb3b5f47bd3cf87882df46
LifeViolation <128378329+LifeViolation@users.noreply.github.com>
2023-07-24 22:28:20 +0900

    add: some function

--- file_util.py
+++ file_util.py
@@ -1,9 +1,11 @@
 import os
+import shutil
 
 
 def initialize() -> None:
     if not os.path.exists("resources"):
-        os.makedirs("./resources")
+        os.makedirs("./resources/input")
+        os.makedirs("./resources/output")
 
 
 def exists_file(path: str) -> bool:
@@ -24,3 +26,20 @@ def is_xlsx(path: str) -> bool:
         return False
     else:
         return True
+
+
+def get_media(path: str) -> bool:
+    file_name: str = __get_file_name(path)
+    __copy_file(path, file_name)
+
+
+def __get_file_name(path: str) -> str:
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+def __copy_file(path: str, file_name: str) -> None:
+    copy_name: str = os.path.join(
+        os.getcwd(), f"resources{os.sep}input{os.sep}{file_name}.zip"
+    )
+
+    shutil.copy(path, copy_name)
